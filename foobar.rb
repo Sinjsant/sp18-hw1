1e5b31048f87e9e038497154dee6b7427107c59d
Sinjon Santos <sinjsant@gmail.com>
2018-02-14 14:23:08 -0800

    Homework 1 Submission

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,9 @@ class Foobar
   def self.baz(a)
     # Class method, not an instance method
     # Call with `Foobar.baz`
-    # Q4 CODE HERE
+    a = a.map { |a| a.to_i + 2}
+    a = a.uniq.keep_if { |a| a % 2 ==0}
+    a = a.reject {|a| a >= 10}
+    a.reduce(:+)
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,21 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  x = a + b
+  x * x
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  # originally a = a.sort; a.map
+a.sort.map { |a| a + 1}
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+
+"#{first_name} #{last_name}"
 
 end
 
@@ -48,5 +54,9 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  word.chars.map {|letter| values[letter.to_sym]}.reduce(:+)
+
+
+
+
 end
